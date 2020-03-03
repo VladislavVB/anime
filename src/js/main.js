785db58ce6e12fd4fc9e4090902d8b580e9da964
VladislavVB <burdukovskij.vlad@mail.ru>
2020-03-03 13:57:18 +0300

    hilekligth

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,21 @@
-new WOW().init();
+var mySwiper = new Swiper(".swiper-container", {
+    // If loop true set photoswipe - counterEl: false
+    loop: true,
+    /* slidesPerView || auto - if you want to set width by css like flickity.js layout - in this case width:80% by CSS */
+    slidesPerView: "auto",
+    spaceBetween: 7,
+    centeredSlides: true,
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      renderBullet: function(index, className) {
+        return '<span class="' + className + '">' + (index + 1) + "</span>";
+      }
+    },
+     // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      }
+  });
